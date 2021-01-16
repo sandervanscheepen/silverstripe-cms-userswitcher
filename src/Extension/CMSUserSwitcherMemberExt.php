0224f6f8cad872c979cc1cefa89600c27eec36f0
sander <sander@sandervanscheepen.nl>
2021-01-16 15:01:32 +0100

    only admin sees switch to add account to UserSwitcher

--- src/Extension/CMSUserSwitcherMemberExt.php
+++ src/Extension/CMSUserSwitcherMemberExt.php
@@ -1,13 +1,15 @@
 <?php
-	
+
 	namespace SanderVanScheepen\SilverstripeCMSUserSwitcher\Extension;
-	
-	use SilverStripe\Forms\CheckboxField;
+
+	use SilverStripe\Admin\CMSProfileController;
+    use SilverStripe\Forms\CheckboxField;
 	use SilverStripe\Forms\FieldList;
 	use SilverStripe\ORM\DataExtension;
 	use SilverStripe\Security\Member;
+    use SilverStripe\Security\Permission;
 	use function _t;
-	
+
 	/**
 	 * Class CMSUserSwitcherMemberExt
 	 *
@@ -19,23 +21,28 @@
 		private static $db = [
 			'CanBeImpersonatedByAdmin' => 'Boolean',
 		];
-		
+
 		private static $has_one = [];
-		
+
 		private static $has_many = [];
-		
+
 		private static $many_many = [
 		];
-		
+
 		private static $belongs_many_many = [
-		
+
 		];
-		
+
 		private static $defaults = [];
-		
+
 		public function updateCMSFields(FieldList $oFields)
 		{
-			$oFields->push(CheckboxField::create('CanBeImpersonatedByAdmin', _t('SanderVanScheepen\\SilverstripeCMSUserSwitcher\\Extension', 'Show in CMS user switcher for admins')));
+		    if(Permission::check('ADMIN') && ! Controller::curr() instanceof CMSProfileController) {
+                $oFields->push(CheckboxField::create('CanBeImpersonatedByAdmin', _t('SanderVanScheepen\\SilverstripeCMSUserSwitcher\\Extension', 'Show in CMS user switcher for admins')));
+            }
+		    else {
+		        $oFields->removeByName('CanBeImpersonatedByAdmin');
+            }
 		}
-		
+
 	}
