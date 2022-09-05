72b7b29d5309c014163e623cf4c30945ec0f317f
Nicolaas / Sunn Side Up <github@sunnysideup.co.nz>
2022-09-06 08:54:17 +1200

    PATCH: remove indentt

--- src/Controller/Admin/UserSwitcherController.php
+++ src/Controller/Admin/UserSwitcherController.php
@@ -1,73 +1,73 @@
 <?php
 
-    namespace SanderVanScheepen\SilverstripeCMSUserSwitcher\Controller\Admin;
+namespace SanderVanScheepen\SilverstripeCMSUserSwitcher\Controller\Admin;
 
-    use SilverStripe\Admin\LeftAndMain;
-    use SilverStripe\Control\Controller;
-    use SilverStripe\Core\Injector\Injector;
-    use SilverStripe\Security\Group;
-    use SilverStripe\Security\IdentityStore;
-    use SilverStripe\Security\Member;
-    use SilverStripe\Security\Permission;
-    use SilverStripe\Security\Security;
-    use SanderVanScheepen\SilverstripeCMSUserSwitcher\Extension\UserSwitcherDropdownLeftAndMainExt;
-    use function in_array;
-    use function intval;
-    use function serialize;
-    use function var_dump;
+use SilverStripe\Admin\LeftAndMain;
+use SilverStripe\Control\Controller;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Security\Group;
+use SilverStripe\Security\IdentityStore;
+use SilverStripe\Security\Member;
+use SilverStripe\Security\Permission;
+use SilverStripe\Security\Security;
+use SanderVanScheepen\SilverstripeCMSUserSwitcher\Extension\UserSwitcherDropdownLeftAndMainExt;
+use function in_array;
+use function intval;
+use function serialize;
+use function var_dump;
 
-    class UserSwitcherController extends LeftAndMain
+class UserSwitcherController extends LeftAndMain
+{
+    private static $url_segment = 'cmsuserswitcher_xhr';
+    private static $url_rule = '/$Action/$ID/$OtherID';
+    private static $menu_icon_class = 'font-icon-cross-mark';
+    private static $required_permission_codes = false;
+    private static $ignore_menuitem = true;
+
+    //private static $subitem_class = 'Member';
+
+    private static $url_handlers = [
+        '' => 'index',
+    ];
+
+    private static $allowed_actions = [];
+
+    public function canView($member = null)
     {
-        private static $url_segment = 'cmsuserswitcher_xhr';
-        private static $url_rule = '/$Action/$ID/$OtherID';
-        private static $menu_icon_class = 'font-icon-cross-mark';
-        private static $required_permission_codes = false;
-        private static $ignore_menuitem = true;
+        return true;
+    }
 
-        //private static $subitem_class = 'Member';
+    public function index($request)
+    {
+        // admin/cmsuserswitcher_xhr?UserSwitcherMemberID=2&BackURL=
+        $sInputMemberID = $request->requestVar('UserSwitcherMemberID');
 
-        private static $url_handlers = [
-            '' => 'index',
-        ];
+        if ($this->canUserSwitch() === true) {
 
-        private static $allowed_actions = [];
+            if ($sInputMemberID !== null) {
 
-        public function canView($member = null)
-        {
-            return true;
-        }
+                $dlMembers  = UserSwitcherDropdownLeftAndMainExt::getSwitchableMembers();
+                $aMemberIDs = $dlMembers->column('ID');
 
-        public function index($request)
-        {
-            // admin/cmsuserswitcher_xhr?UserSwitcherMemberID=2&BackURL=
-            $sInputMemberID = $request->requestVar('UserSwitcherMemberID');
+                if (in_array(intval($sInputMemberID), $aMemberIDs)) {
 
-            if ($this->canUserSwitch() === true) {
+                    $oMember = Member::get()->byID($sInputMemberID);
+                    if ($oMember) {
+                        $this->getRequest()->getSession()->set('CMSUserSwitched', 1);
+                        $oIdentityStore = Injector::inst()->get(IdentityStore::class);
+                        $oIdentityStore->logIn($oMember, false, $this->getRequest());
 
-                if ($sInputMemberID !== null) {
-
-                    $dlMembers  = UserSwitcherDropdownLeftAndMainExt::getSwitchableMembers();
-                    $aMemberIDs = $dlMembers->column('ID');
-
-                    if (in_array(intval($sInputMemberID), $aMemberIDs)) {
-
-                        $oMember = Member::get()->byID($sInputMemberID);
-                        if ($oMember) {
-                            $this->getRequest()->getSession()->set('CMSUserSwitched', 1);
-                            $oIdentityStore = Injector::inst()->get(IdentityStore::class);
-                            $oIdentityStore->logIn($oMember, false, $this->getRequest());
-
-                            return $this->redirectBack();
-                        }
-                    }
-                    else {
                         return $this->redirectBack();
                     }
                 }
+                else {
+                    return $this->redirectBack();
+                }
             }
-
-            return $this->getResponseNegotiator()->respond($request);
         }
 
-
+        return $this->getResponseNegotiator()->respond($request);
     }
+
+
+}
